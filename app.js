111a6fcd569233d1bbf5e9a91191ebbeb5297c8f
capt-alien <eric.botcher@presidio.edu>
2019-01-09 17:18:29 -0800

    users can create articles

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const app = express()
 
 var exphbs = require('express-handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let articles = [
+  { name: "batman forever", description: "This is clearly trash", bin:"trash" },
+  { name: "ATitanic", description: "This is deffinetly recycleing" , bin:"compost" }
+]
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Routeing
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('articles-index', { articles: articles });
+})
+
+// INDEX
+app.get('/articles', (req, res) => {
+  res.render('articles-index', { articles: articles });
 })
 
 app.listen(3000, () => {
