0dec5c99efe159e2e53fb14ff142e3705bb14bb0
capt-alien <eric.botcher@presidio.edu>
2019-01-10 13:59:39 -0800

    added mongoDB

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 const express = require('express')
 const app = express()
 
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/zwaste');
+
+const Article = mongoose.model('Article', {
+  name: String,
+  description: String,
+  bin: String
+});
+
 var exphbs = require('express-handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
-let articles = [
-  { name: "batman forever", description: "This is clearly trash", bin:"trash" },
-  { name: "ATitanic", description: "This is deffinetly recycleing" , bin:"compost" }
-]
+// let articles = [
+//   { name: "batman forever", description: "This is clearly trash", bin:"trash" },
+//   { name: "ATitanic", description: "This is deffinetly recycleing" , bin:"compost" }
+// ]
+
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Routeing
+// INDEX
 app.get('/', (req, res) => {
-  res.render('articles-index', { articles: articles });
+  Article.find()
+    .then(articles => {
+      res.render('articles-index', { articles: articles });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
-// INDEX
-app.get('/articles', (req, res) => {
-  res.render('articles-index', { articles: articles });
-})
+// INDEX Page for later devlopment
+// app.get('/articles', (req, res) => {
+//   res.render('articles-index', { articles: articles });
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
