5f5e07b8995b314cf2ab7a23923c5439771ac287
capt-alien <eric.botcher@presidio.edu>
2019-01-10 14:45:16 -0800

    users can create articles

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/zwaste');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
 const Article = mongoose.model('Article', {
   name: String,
   description: String,
@@ -20,11 +23,16 @@ var exphbs = require('express-handlebars');
 // ]
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Routeing
+// INDEX Page for later devlopment
+// app.get('/articles', (req, res) => {
+//   res.render('articles-index', { articles: articles });
+// })
+
 // INDEX
 app.get('/', (req, res) => {
   Article.find()
@@ -36,10 +44,22 @@ app.get('/', (req, res) => {
     })
 })
 
-// INDEX Page for later devlopment
-// app.get('/articles', (req, res) => {
-//   res.render('articles-index', { articles: articles });
-// })
+// NEW
+app.get('/articles/new', (req, res) => {
+  res.render('articles-new', {});
+})
+
+
+// CREATE
+app.post('/articles', (req, res) => {
+  Article.create(req.body).then((article) => {
+    console.log(article);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
